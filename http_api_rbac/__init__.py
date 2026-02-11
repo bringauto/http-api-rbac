5929d6a7c5125ce51442485196afa7136e149510
Ondrej Vala <93932164+ondrejvala2@users.noreply.github.com>
2026-02-11 13:05:07 +0100

    feat: add backwards-compatible aliases for Authentication and Authorization classes

--- http_api_rbac/__init__.py
+++ http_api_rbac/__init__.py
@@ -2,3 +2,7 @@ __version__ = "0.1.0"
 
 from http_api_rbac.authentication import Authentication
 from http_api_rbac.authorization import Authorization
+
+# Backwards-compatible aliases for the old class names
+AuthenticationObj = Authentication
+AuthorizationObj = Authorization
